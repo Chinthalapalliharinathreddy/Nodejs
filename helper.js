8393a265d6599e1de90b36cf5d3dfa19563ac450
Chinthalapalliharinathreddy <chinthalapalli.harinathreddy@gmail.com>
2025-07-10 14:16:50 +0530

    first commit

---
+++ helper.js
@@ -0,0 +1,10 @@
+function greet(name){
+    console.log(`Hello ${name}`)
+}
+function addOne(num){
+console.log(num+1)
+}
+module.exports={
+    greet,
+    addOne,
+}
---
+++ timer.js
@@ -0,0 +1,14 @@
+//Timer Function
+
+//1.TimeOut function
+console.log("Hello Hari")
+
+// setTimeout(()=>{
+//     console.log("Run after 2 seconds")
+// },2000)
+
+
+//2.Interval Function
+setInterval(()=>{
+    console.log("Running this...")
+},1000)
